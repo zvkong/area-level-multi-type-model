ce0defa5343aac449330fd2629532ada460856b9
Zewei Kong <34445713+zvkong@users.noreply.github.com>
2026-03-16 11:13:30 -0500

    Add files via upload

--- build_SD_cleaned.R
+++ build_SD_cleaned.R
@@ -1,31 +1,39 @@
-suppressPackageStartupMessages({
-  library(tidycensus)
-  library(dplyr)
-  library(sf)
-})
+library(tidycensus)
+library(dplyr)
+library(sf)
 
-year <- 2021
-state <- "SD"
-moe_mult <- qnorm(0.95)
+state_code <- "SD"
+year_use <- 2021
+z_90 <- qnorm(0.95)
+eps <- 1e-06
 
 income21.ed <-
   get_acs(
     geography = "tract",
     variables = "B19013_001",
     summary_var = "B01003_001",
-    state = state,
+    state = state_code,
     geometry = FALSE,
-    year = year,
-    survey = "acs5"
+    year = year_use
   ) %>%
   arrange(GEOID) %>%
-  rename(population = summary_est, medium_income = estimate, income_moe = moe) %>%
-  select(GEOID, NAME, population, medium_income, income_moe) %>%
-  filter(population > 0, !is.na(medium_income), !is.na(income_moe), medium_income > 0) %>%
-  mutate(
-    var_income = (income_moe / moe_mult)^2,
+  transmute(
+    GEOID,
+    NAME,
+    population = summary_est,
+    medium_income = estimate,
+    moe,
+    var_income = (moe / z_90)^2,
     lg_income = log(medium_income),
-    lg_var = var_income / medium_income^2
+    lg_var = (moe / z_90)^2 / (medium_income^2)
+  ) %>%
+  filter(
+    population > 0,
+    !is.na(medium_income),
+    !is.na(moe),
+    medium_income > 0,
+    is.finite(lg_income),
+    is.finite(lg_var)
   )
 
 povrate21 <-
@@ -33,74 +41,73 @@ povrate21 <-
     geography = "tract",
     variables = "B17001_002",
     summary_var = "B01003_001",
-    state = state,
+    state = state_code,
     geometry = TRUE,
-    year = year,
-    survey = "acs5"
+    year = year_use
   ) %>%
   arrange(GEOID) %>%
-  rename(population = summary_est, pov_count = estimate, pov_moe = moe) %>%
+  transmute(
+    GEOID,
+    NAME,
+    population = summary_est,
+    pov_count = estimate,
+    pov_moe = moe,
+    geometry
+  ) %>%
   filter(population > 0, !is.na(pov_count), !is.na(pov_moe)) %>%
   mutate(
-    pov_rate = pov_count / population,
-    var_pov_count = (pov_moe / moe_mult)^2,
-    var_pov = (pov_moe / (population * moe_mult))^2,
-    m = pov_rate * (1 - pov_rate) / pmax(var_pov, 1e-12),
-    ystar = m * pov_rate
+    var_pov_count = (pov_moe / z_90)^2,
+    var_pov = (pov_moe / (population * z_90))^2,
+    pov_rate = pmin(pmax(pov_count / population, eps), 1 - eps),
+    m = pov_rate * (1 - pov_rate) / pmax(var_pov, 1e-12)
   ) %>%
-  select(GEOID, NAME, population, pov_rate, m, var_pov, var_pov_count, pov_count, ystar, geometry)
+  filter(is.finite(pov_rate), is.finite(var_pov), is.finite(m), m > 0)
 
 white_df <-
   get_acs(
     geography = "tract",
     variables = "B01001A_001",
-    summary_var = "B01003_001",
-    state = state,
+    state = state_code,
     geometry = FALSE,
-    year = year,
-    survey = "acs5"
+    year = year_use
   ) %>%
   arrange(GEOID) %>%
   transmute(GEOID, white = estimate)
 
-aligned <-
+aligned_sf <-
   povrate21 %>%
   inner_join(income21.ed, by = "GEOID", suffix = c("", "_inc")) %>%
   inner_join(white_df, by = "GEOID") %>%
-  select(-NAME_inc, -population_inc) %>%
-  mutate(X_white = white / population) %>%
+  mutate(
+    NAME = coalesce(NAME, NAME_inc),
+    X_white = white / population
+  ) %>%
   filter(
-    is.finite(lg_income),
-    is.finite(lg_var),
-    is.finite(pov_rate),
-    is.finite(var_pov),
-    is.finite(m),
-    m > 0,
-    !is.na(X_white)
+    !is.na(X_white),
+    is.finite(X_white)
   ) %>%
   arrange(GEOID)
 
-lg_mean <- mean(aligned$lg_income)
-lg_sd <- sd(aligned$lg_income)
-
-aligned <-
-  aligned %>%
-  mutate(
-    income_scaled = (lg_income - lg_mean) / lg_sd,
-    lg_var = lg_var / lg_sd^2
+income21.ed <-
+  aligned_sf %>%
+  st_drop_geometry() %>%
+  transmute(
+    GEOID,
+    NAME,
+    population,
+    medium_income,
+    moe,
+    var_income,
+    lg_income,
+    lg_var,
+    income_scaled = as.numeric(scale(lg_income))
   )
 
-MO_county_sf <- aligned %>% select(GEOID, NAME, geometry)
-
-income21.ed <-
-  aligned %>%
-  st_drop_geometry() %>%
-  select(GEOID, NAME, population, medium_income, income_moe, var_income, lg_income, lg_var, income_scaled)
-
 povrate21 <-
-  aligned %>%
-  select(GEOID, NAME, population, pov_rate, m, var_pov, var_pov_count, pov_count, ystar, geometry)
+  aligned_sf %>%
+  select(GEOID, NAME, population, pov_rate, m, var_pov, var_pov_count, pov_count, geometry)
 
-X_white <- aligned$X_white
+MO_county_sf <- povrate21 %>% select(GEOID, NAME, geometry)
+X_white <- aligned_sf$X_white
 
 save(MO_county_sf, income21.ed, povrate21, X_white, file = "SD cleaned.RData")
--- build_westnorthc.R
+++ build_westnorthc.R
@@ -1,202 +1,163 @@
-suppressPackageStartupMessages({
-  library(tidycensus)
-  library(dplyr)
-  library(sf)
-  library(purrr)
-  library(spdep)
-  library(Matrix)
-})
+library(tidycensus)
+library(dplyr)
+library(sf)
+library(purrr)
+library(spdep)
 
-year <- 2021
-states <- c("IA", "KS", "MN", "MO", "NE", "ND", "SD")
-moe_mult <- qnorm(0.95)
+states_use <- c("IA", "KS", "MN", "MO", "NE", "ND", "SD")
+year_use <- 2021
+z_90 <- qnorm(0.95)
+eps <- 1e-06
 
-large_abs <- function(x, threshold = 0.25) {
-  which(abs(x) > threshold * max(abs(x), na.rm = TRUE))
+fetch_acs_states <- function(variables, summary_var = NULL, geometry = FALSE, survey = "acs5") {
+  map_dfr(
+    states_use,
+    ~ get_acs(
+      geography = "tract",
+      variables = variables,
+      summary_var = summary_var,
+      state = .x,
+      geometry = geometry,
+      year = year_use,
+      survey = survey
+    )
+  )
 }
 
-get_income_state <- function(state) {
-  get_acs(
-    geography = "tract",
+income21.ed <-
+  fetch_acs_states(
     variables = "B19013_001",
     summary_var = "B01003_001",
-    state = state,
-    geometry = FALSE,
-    year = year,
-    survey = "acs5"
+    geometry = FALSE
   ) %>%
-    transmute(
-      GEOID,
-      NAME,
-      population = summary_est,
-      medium_income = estimate,
-      income_moe = moe
-    )
-}
-
-get_poverty_state <- function(state) {
-  get_acs(
-    geography = "tract",
-    variables = "B17001_002",
-    summary_var = "B01003_001",
-    state = state,
-    geometry = TRUE,
-    year = year,
-    survey = "acs5"
-  ) %>%
-    transmute(
-      GEOID,
-      NAME,
-      population = summary_est,
-      pov_count = estimate,
-      pov_moe = moe,
-      geometry
-    )
-}
-
-get_white_state <- function(state) {
-  get_acs(
-    geography = "tract",
-    variables = "B01001A_001",
-    summary_var = "B01003_001",
-    state = state,
-    geometry = FALSE,
-    year = year,
-    survey = "acs5"
-  ) %>%
-    transmute(GEOID, white = estimate)
-}
-
-get_bachelor_state <- function(state) {
-  get_acs(
-    geography = "tract",
-    variables = c(
-      total25 = "B15003_001",
-      bachelor = "B15003_022",
-      master = "B15003_023",
-      professional = "B15003_024",
-      doctorate = "B15003_025"
-    ),
-    state = state,
-    geometry = FALSE,
-    year = year,
-    survey = "acs5"
-  ) %>%
-    select(GEOID, variable, estimate) %>%
-    tidyr::pivot_wider(names_from = variable, values_from = estimate) %>%
-    mutate(bachelor_plus = bachelor + master + professional + doctorate) %>%
-    transmute(GEOID, adults25 = total25, bachelor_plus)
-}
-
-get_snap_state <- function(state) {
-  get_acs(
-    geography = "tract",
-    variables = c(total_hh = "B22001_001", snap_hh = "B22001_002"),
-    state = state,
-    geometry = FALSE,
-    year = year,
-    survey = "acs5"
-  ) %>%
-    select(GEOID, variable, estimate) %>%
-    tidyr::pivot_wider(names_from = variable, values_from = estimate) %>%
-    transmute(GEOID, total_hh, snap_hh)
-}
-
-income21.ed <- map_dfr(states, get_income_state) %>%
   arrange(GEOID) %>%
-  filter(population > 0, !is.na(medium_income), !is.na(income_moe), medium_income > 0) %>%
-  mutate(
-    var_income = (income_moe / moe_mult)^2,
+  transmute(
+    GEOID,
+    NAME,
+    population = summary_est,
+    medium_income = estimate,
+    moe,
+    var_income = (moe / z_90)^2,
     lg_income = log(medium_income),
-    lg_var = var_income / medium_income^2
+    lg_var = (moe / z_90)^2 / (medium_income^2)
+  ) %>%
+  filter(
+    population > 0,
+    !is.na(medium_income),
+    !is.na(moe),
+    medium_income > 0,
+    is.finite(lg_income),
+    is.finite(lg_var)
   )
 
-povrate21 <- map_dfr(states, get_poverty_state) %>%
-  st_as_sf() %>%
+povrate21 <-
+  fetch_acs_states(
+    variables = "B17001_002",
+    summary_var = "B01003_001",
+    geometry = TRUE
+  ) %>%
   arrange(GEOID) %>%
+  transmute(
+    GEOID,
+    NAME,
+    population = summary_est,
+    pov_count = estimate,
+    pov_moe = moe,
+    geometry
+  ) %>%
   filter(population > 0, !is.na(pov_count), !is.na(pov_moe)) %>%
   mutate(
-    pov_rate = pov_count / population,
-    var_pov_count = (pov_moe / moe_mult)^2,
-    var_pov = (pov_moe / (population * moe_mult))^2,
-    m = pov_rate * (1 - pov_rate) / pmax(var_pov, 1e-12),
-    ystar = m * pov_rate
+    var_pov_count = (pov_moe / z_90)^2,
+    var_pov = (pov_moe / (population * z_90))^2,
+    pov_rate = pmin(pmax(pov_count / population, eps), 1 - eps),
+    m = pov_rate * (1 - pov_rate) / pmax(var_pov, 1e-12)
   ) %>%
-  select(GEOID, NAME, population, pov_rate, m, var_pov, var_pov_count, pov_count, ystar, geometry)
+  filter(is.finite(pov_rate), is.finite(var_pov), is.finite(m), m > 0)
 
-white_df <- map_dfr(states, get_white_state) %>% arrange(GEOID)
-bachelor_df <- map_dfr(states, get_bachelor_state) %>% arrange(GEOID)
-snap_df <- map_dfr(states, get_snap_state) %>% arrange(GEOID)
+white_df <-
+  fetch_acs_states(
+    variables = "B01001A_001",
+    geometry = FALSE
+  ) %>%
+  arrange(GEOID) %>%
+  transmute(GEOID, white = estimate)
 
-aligned <-
+bachelor_df <-
+  fetch_acs_states(
+    variables = "DP02_0068PE",
+    geometry = FALSE,
+    survey = "acs5/profile"
+  ) %>%
+  arrange(GEOID) %>%
+  transmute(GEOID, X_bachelor = estimate / 100)
+
+snap_df <-
+  fetch_acs_states(
+    variables = "DP03_0074PE",
+    geometry = FALSE,
+    survey = "acs5/profile"
+  ) %>%
+  arrange(GEOID) %>%
+  transmute(GEOID, X_snap = estimate / 100)
+
+aligned_sf <-
   povrate21 %>%
   inner_join(income21.ed, by = "GEOID", suffix = c("", "_inc")) %>%
   inner_join(white_df, by = "GEOID") %>%
   inner_join(bachelor_df, by = "GEOID") %>%
   inner_join(snap_df, by = "GEOID") %>%
-  select(-NAME_inc, -population_inc) %>%
   mutate(
-    X_white = white / population,
-    X_bachelor = ifelse(adults25 > 0, bachelor_plus / adults25, NA_real_),
-    X_snap = ifelse(total_hh > 0, snap_hh / total_hh, NA_real_)
+    NAME = coalesce(NAME, NAME_inc),
+    X_white = white / population
   ) %>%
   filter(
-    is.finite(lg_income),
-    is.finite(lg_var),
-    is.finite(pov_rate),
-    is.finite(var_pov),
-    is.finite(m),
-    m > 0,
+    !is.na(X_white),
+    !is.na(X_bachelor),
+    !is.na(X_snap),
     is.finite(X_white),
     is.finite(X_bachelor),
     is.finite(X_snap)
   ) %>%
   arrange(GEOID)
 
-lg_mean <- mean(aligned$lg_income)
-lg_sd <- sd(aligned$lg_income)
+povrate21 <-
+  aligned_sf %>%
+  select(GEOID, NAME, population, pov_rate, m, var_pov, var_pov_count, pov_count, geometry)
 
-aligned <-
-  aligned %>%
-  mutate(
-    income_scaled = (lg_income - lg_mean) / lg_sd,
-    lg_var = lg_var / lg_sd^2
+income21.ed <-
+  aligned_sf %>%
+  st_drop_geometry() %>%
+  transmute(
+    GEOID,
+    NAME,
+    population,
+    medium_income,
+    moe,
+    var_income,
+    lg_income,
+    lg_var,
+    income_scaled = as.numeric(scale(lg_income))
   )
 
-MO_county_sf <- aligned %>% select(GEOID, NAME, geometry)
-
-nb_obj <- poly2nb(MO_county_sf, queen = TRUE)
-W_list <- nb2listw(nb_obj, style = "B", zero.policy = TRUE)
-B <- as.matrix(as_dgRMatrix_listw(W_list))
-eig <- eigen(B, symmetric = TRUE)
-index <- large_abs(eig$values, 0.25)
-S <- eig$vectors[, index, drop = FALSE]
-
-X_white <- aligned$X_white
-X_bachelor <- aligned$X_bachelor
-X_snap <- aligned$X_snap
+MO_county_sf <- povrate21 %>% select(GEOID, NAME, geometry)
+X_white <- aligned_sf$X_white
+X_bachelor <- aligned_sf$X_bachelor
+X_snap <- aligned_sf$X_snap
 X_1 <- as.matrix(cbind(1, X_bachelor, X_snap, X_white))
 X_2 <- as.matrix(cbind(1, X_white, X_snap, X_bachelor))
 
-income21.ed <-
-  aligned %>%
-  st_drop_geometry() %>%
-  select(GEOID, NAME, population, medium_income, income_moe, var_income, lg_income, lg_var, income_scaled)
+if (file.exists("functions.r")) {
+  source("functions.r")
+}
+if (!exists("large_abs", mode = "function")) {
+  large_abs <- function(x, c) which(abs(x) > quantile(abs(x), probs = c, na.rm = TRUE))
+}
 
-povrate21 <-
-  aligned %>%
-  select(GEOID, NAME, population, pov_rate, m, var_pov, var_pov_count, pov_count, ystar, geometry)
+MO_nb <- poly2nb(MO_county_sf, queen = TRUE)
+W_list <- nb2listw(MO_nb, style = "B", zero.policy = TRUE)
+B <- as.matrix(as_dgRMatrix_listw(W_list))
+eig <- eigen(B, symmetric = TRUE)
+idx <- large_abs(eig$values, 0.25)
+S <- eig$vectors[, idx, drop = FALSE]
 
-save(
-  MO_county_sf,
-  income21.ed,
-  povrate21,
-  B,
-  X_white,
-  X_bachelor,
-  X_snap,
-  X_1,
-  X_2,
-  S,
-  file = "westnorthc.Rdata"
-)
+save(MO_county_sf, income21.ed, povrate21, X_white, X_bachelor, X_snap, X_1, X_2, B, S, file = "westnorthc.Rdata")
